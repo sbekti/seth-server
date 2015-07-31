1d4aebcb48743e8d43233327aa5afc82568ca4bf
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-08-01 01:11:12 +0700

    Change POST to GET

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var sockjs = require('sockjs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/location', function(req, res) {
-  var data = req.body;
+app.get('/api/v1/location', function(req, res) {
+  var data = req.query;
   console.log(data);
   res.json(data);
 });
