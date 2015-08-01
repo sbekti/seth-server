2f7b2f3463657c2d1b180ca40f0460a74ad1a0fe
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-08-01 21:43:39 +0700

    Removed GET endpoint

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/api/v1/location', function(req, res) {
   var data = req.body;
   console.log(data);
-  res.json(data);
-});
-
-app.get('/api/v1/location', function(req, res) {
-  var data = req.query;
-  console.log(data);
-  res.json(data);
+  res.send(OK);
 });
 
 var echo = sockjs.createServer({
